97eae671603d06625f8bc20486245442c9767f1d
TyoGantengz <notiiionew@gmail.com>
2025-10-24 21:32:13 +0700

    prime

--- PYHTON EXERCISE/python.py
+++ PYHTON EXERCISE/python.py
@@ -1,2 +1,18 @@
 print("Hello World")
 print("Welcome to Python programming!")
+
+print("Lets try by calculating prime numbers!")
+
+n = int(input("Enter a number: "))
+if n < 2:
+    print(n, "is not a prime number.")
+else:
+    prima = True
+    for i in range(2, n):
+        if n % i == 0:
+            prima = False
+            break
+if prima:
+    print(n, "is a prime number.")
+else:
+    print(n, "is not a prime number.")
