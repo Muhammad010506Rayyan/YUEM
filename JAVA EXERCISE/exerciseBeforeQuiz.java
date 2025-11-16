449605b7df05ac71f09df4c00cbd7d518ed4eddf
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-16 19:07:06 +0700

    even and odd using while

--- JAVA EXERCISE/exerciseBeforeQuiz.java
+++ JAVA EXERCISE/exerciseBeforeQuiz.java
@@ -10,15 +10,16 @@ public class exerciseBeforeQuiz{
     System.out.print("input the maximum ");
     int max=sc.nextInt();
     System.out.print("");
-//make the conditional using loop for
-for(int i=min;i<max;i++){
+//make the conditional using loop while
+int i=min;
+while(i<max){
     if(i%2==0){
-        System.out.println((i)+" is even");
+        System.out.println((i)+ " is even");
     }else{
         System.out.println((i)+" is odd");
     }
+    i++;
 }
-
 
     }
 }
