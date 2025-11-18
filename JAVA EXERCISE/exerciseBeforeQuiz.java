fa625f78c7cde4a48764f25ed98df5a5a57821dd
Muhammad_Rayyan <hairay261@gmail.com>
2025-11-18 17:31:33 +0700

    bikin loop row and column

--- JAVA EXERCISE/exerciseBeforeQuiz.java
+++ JAVA EXERCISE/exerciseBeforeQuiz.java
@@ -5,23 +5,24 @@ import java.util.Arrays;
 public class exerciseBeforeQuiz{
     public static void main(String[]args){
     Scanner sc=new Scanner(System.in);
-    System.out.println("===Study Hour===");
-    int hour;
-    int total=0;
-    //make the loop
-    int day=1;
-    while(day<=7){
-    System.out.println("how long you study in day "+(day));
-    hour=sc.nextInt();
-    System.out.println(" ");
-      if(hour>=10){
-        while (hour>=10){ 
-        System.out.println("enter again the value, not equals or more to 10");
-        hour=sc.nextInt();    
-        }}
-    total += hour;
-    day++;      
-}
-    System.out.println("total= "+total);
+    System.out.println("===Test row and Column===");
+    int rows=2;
+    int column=5;
+    int[][] angka= new int[rows][column];
+    for(int i=0;i<rows;i++){
+        System.out.println("untuk baris "+(i+1)+".");
+    for(int j=0;j<column;j++){
+        System.out.print((j+1)+".");
+
+        angka[i][j]=sc.nextInt();    
+        }
+    }
+    for(int i=0;i<rows;i++){
+    for(int j=0;j<column;j++){
+        System.out.print(angka[i][j]+" ");
+    } 
+        System.out.println(" ");
+}        
+
     }
 }
